33cad428804d6829de5c3961ba854b97d526e6af
ashok3597 <61827140+ashok3597@users.noreply.github.com>
2020-07-06 16:44:28 +0530

    Update mycode.py

--- ashok3597/mycode.py
+++ ashok3597/mycode.py
@@ -25,9 +25,12 @@ def cli():
 @snapshots.command('list')
 @click.option('--project', default = None, help = "Only Snapshots for the project (tag Project:<name>)")
 
-def list_snapshots(project):
+@click.option('--all','list_all', default = False, is_flag=True, help = "List all snapshot for all volume, not just the most recent'
+
+def list_snapshots(project,list_all):
     "List of Volume snapshots"
     
+
     instances = filter_instances(project)
 
 	for i in instances:
@@ -41,7 +44,8 @@ def list_snapshots(project):
 					s.progress,
 					s.start_time.strftime("%c")
 				)))
-				if s.state = 'completed': break				
+
+				if s.state = 'completed' and not list_all: break 
 	return
 
 @cli.group('volumes')
